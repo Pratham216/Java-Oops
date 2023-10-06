b8be613d2ee0bcfcf4bfd3ac7df3f84241bf41db
Pratham216 <prathambisht282@gmail.com>
2023-10-06 09:19:32 +0530

    Third day

---
+++ S02 L02 Making decission with operators/src/data1.java
@@ -0,0 +1,15 @@
+public class data1 {
+
+		public static void main(String[] args) {
+			int x = 6, y = 7;
+			
+	           if(x >= y){
+	        	   System.out.println("true");
+	           }else{
+	        	   System.out.println("false");
+	           }
+		}
+
+	
+
+}
