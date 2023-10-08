b90d4b763464cd67dd2990ee59603c0a0a71d273
Pratham216 <prathambisht282@gmail.com>
2023-10-08 10:35:32 +0530

    Switch case

---
+++ S03 L01 Switch case/src/data3.java
@@ -0,0 +1,24 @@
+
+public class data3 {
+	public static void main(String[] args) {
+		String name = "Editor";
+
+		switch (name.toLowerCase()) {
+		case "author":
+			System.out.println("Chaand");
+			break;
+		case "team":
+			System.out.println("Team StudyEasy");
+			break;
+
+		case "editor":
+			System.out.println("Shabaz & Salim");
+			break;
+		default:
+			System.out.println("Invalid entry");
+			break;
+
+		}
+
+	}
+}
