c751ab92f24340f741a24a70472319c2b18a8244
Pratham216 <prathambisht282@gmail.com>
2023-10-12 21:23:35 +0530

    Inheritance Part-3

---
+++ S05 L07 Inheritance 3/src/child/Bike.java
@@ -0,0 +1,31 @@
+package child;
+
+import parent.Vehicle;
+
+public class Bike extends Vehicle {
+	private String handle;
+
+	public Bike() {
+		super();
+		this.handle="long";
+	}
+
+	public Bike(String handle,String engine, int wheels, int seats, int fuelTank, String lights) {
+		super(engine,wheels,seats,fuelTank,lights);
+		this.handle = handle;
+	}
+
+	public String getHandle() {
+		return handle;
+	}
+
+	@Override
+	public String toString() {
+		return "Bike [getEngine()=" + getEngine() + ", getWheels()=" + getWheels() + ", getSeats()=" + getSeats()
+				+ ", getFuelTank()=" + getFuelTank() + ", getLights()=" + getLights() + "]";
+	}
+	public void run() {
+		System.out.println("Bike is running");
+		System.out.println(toString());
+	}
+}
---
+++ S05 L07 Inheritance 3/src/main/Demo.java
@@ -0,0 +1,10 @@
+package main;
+import child.Bike;
+public class Demo {
+
+	public static void main(String[] args) {
+		Bike hayabuza= new Bike();
+		hayabuza.run();
+	}
+
+}
