7b3bd325b55f1039752b3f22f1157314b323af69
Pratham216 <prathambisht282@gmail.com>
2023-10-15 10:24:31 +0530

    Static elements

---
+++ S06 L06 static elements/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Current value is : "+data.getStaticVariable());
+		data.setStaticVariable(50);
+		System.out.println("Current value is : "+data.getStaticVariable());
+		data.setStaticVariable(100);
+		System.out.println("Current value is : "+data.getStaticVariable());
+		
+	}
+
+}
---
+++ S06 L06 static elements/src/data.java
@@ -0,0 +1,15 @@
+
+public class data {
+	static int staticVariable =0;
+
+	public static int getStaticVariable() {
+		return staticVariable;
+	}
+
+	public static void setStaticVariable(int staticVariable) {
+		data.staticVariable = staticVariable;
+	}
+	
+	
+
+}
