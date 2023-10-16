57ea6a7a2ee2ad3f87d4aa2330b0b255e8707327
Pratham216 <prathambisht282@gmail.com>
2023-10-16 21:55:55 +0530

    Arrays started

---
+++ S07 L01 Array initialization/src/data.java
@@ -0,0 +1,24 @@
+
+public class data {
+
+	public static void main(String[] args) {
+		
+		int[] arr = new int[5];
+		arr[0]= 12;
+		arr[1]= 33;
+		arr[2]= 44;
+		arr[3]= 55;
+		arr[4]= 66;
+		for ( int i = 0; i < arr.length; i++) {
+		    System.out.print(arr[i]+",");
+		}
+		
+		System.out.println("\n");
+		
+		int[] array2 = {1,2,3,4,5,6,7,8,9};
+		for ( int i = 0; i < array2.length; i++) {
+		    System.out.print(array2[i]+" ");
+		}
+	}
+
+}
